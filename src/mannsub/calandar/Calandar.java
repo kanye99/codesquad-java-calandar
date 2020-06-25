b8962b2c9e2c9b0c2c0d95863f5c53aa42fee8c9
mannsub <mannsub@192.168.0.9>
2020-06-26 01:42:27 +0900

    Init Caladar

---
+++ src/mannsub/calandar/Calandar.java
@@ -0,0 +1,11 @@
+package mannsub.calandar;
+
+public class Calandar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Caladar");
+
+	}
+
+}
